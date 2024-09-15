f87dfe4851b502bb224175a8c746071a53e3970e
Kpeeris <kpeeris@student.unimelb.edu.au>
2024-09-15 22:45:32 +1000

    modified navbar ui using tailwind and replaced bootstrap dropdown with shadcn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import './Navbar.css';
-import DropdownMenu from "./buttons/Dropdown";
+import "./Navbar.css";
+import Dropdown from "./buttons/Dropdown";
+import logo_horizontal from "../assets/icons/logo_horizontal.svg";
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav>
-                <Link to='/' className="title">SDG Scholar</Link>
-                <ul>
-                    <li><NavLink to='/about'>About</NavLink></li>
-                    <li><NavLink to='/faq'>FAQ</NavLink></li>
-                    <DropdownMenu />
-                    <li><NavLink to='/discussion'>Discussion</NavLink></li>
-                </ul>
+  return (
+    <div>
+      {/* navbar container */}
+      <nav className="flex items-center justify-between w-full h-16">
+        {/* logo */}
+        <Link to="/" className="ml-12">
+          <img src={logo_horizontal} alt="SDG logo" className="w-40 h-10" />
+        </Link>
 
-            </nav>
+        {/* Navigation links */}
+        <ul className="absolute left-1/2 transform -translate-x-1/2 flex">
+          <li className="border-r border-gray-300 px-2">
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li className="border-r border-gray-300 px-4">
+            <NavLink to="/faq">FAQ</NavLink>
+          </li>
+          <Dropdown className="border-r border-gray-300 px-4" />
+          <li className="border-r border-gray-300 px-4">
+            <NavLink to="/discussion">Discussion</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/components/buttons/Dropdown.jsx
+++ src/components/buttons/Dropdown.jsx
@@ -1,117 +1,155 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import "./Dropdown.css";
-import Dropdown from 'react-bootstrap/Dropdown';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+//import "./Dropdown.css";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-function DropdownMenu() {
-    return (
-        <Dropdown>
-            <Dropdown.Toggle className="custom-dropdown-toggle" id="dropdown-basic">
-                Goals
-            </Dropdown.Toggle>
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-            <Dropdown.Menu className='scrollable-dropdown'>
-                {goalsDropdown.map((item, index) => {
-                    return (
-                        <Dropdown.Item as={Link} to={item.path} key={index}>
-                            {item.title}
-                        </Dropdown.Item>
-                    )
-                })}
-            </Dropdown.Menu>
-        </Dropdown>
-    );
+// import Dropdown from 'react-bootstrap/Dropdown';
+
+function Dropdown() {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleNavigation = (path) => {
+    if (path) {
+      navigate(path); // Redirect to the specified path
+    }
+  };
+  return (
+    <DropdownMenu>
+      {/* dropdown button */}
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Goals</Button>
+      </DropdownMenuTrigger>
+
+      {/* dropdown list */}
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>SDG Goals</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <ScrollArea className="h-72">
+          {goalsDropdown.map(({ id, title, path }) => {
+            return (
+              <DropdownMenuItem key={id} onClick={() => handleNavigation(path)}>
+                {title}
+              </DropdownMenuItem>
+            );
+          })}
+        </ScrollArea>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 const goalsDropdown = [
-    {
-        id: 1,
-        title: "1 No Poverty",
-        path: "",
-        cName: "submenu-item",
-    },
-    {
-        id: 2,
-        title: "2 Zero Hunger",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 3,
-        title: "3 Good Health and Well-being",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 4,
-        title: "4 Quality Education",
-        path: "",
-        cName: "submenu-item",
-    },
-    {
-        id: 5,
-        title: "5 Gender Equality",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 6,
-        title: "6 Clean Water and ",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 7,
-        title: "7 Affordable and Clean Energy",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 8,
-        title: "8 Decent Work and Economic Growth",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 9,
-        title: "9 Industry, Innovation and Infrastructure",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 10,
-        title: "10 Reduce Inequalities",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 11,
-        title: "11 Sustainable Cities and Communities",
-        path: '/sdg11',
-        cName: "submenu-item",
-    }, {
-        id: 12,
-        title: "12 Responsible Consumption and Production",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 13,
-        title: "13 Climate Action",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 14,
-        title: "14 Life Below Water",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 15,
-        title: "15 Life on Land",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 16,
-        title: "16 Peace, Justice and Strong Institutions",
-        path: "",
-        cName: "submenu-item",
-    }, {
-        id: 17,
-        title: "17 Partnerships for the Goals",
-        path: "",
-        cName: "submenu-item",
-    }
+  {
+    id: 1,
+    title: "1 No Poverty",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "2 Zero Hunger",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "3 Good Health and Well-being",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "4 Quality Education",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 5,
+    title: "5 Gender Equality",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 6,
+    title: "6 Clean Water and ",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 7,
+    title: "7 Affordable and Clean Energy",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 8,
+    title: "8 Decent Work and Economic Growth",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 9,
+    title: "9 Industry, Innovation and Infrastructure",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 10,
+    title: "10 Reduce Inequalities",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 11,
+    title: "11 Sustainable Cities and Communities",
+    path: "/sdg11",
+    cName: "submenu-item",
+  },
+  {
+    id: 12,
+    title: "12 Responsible Consumption and Production",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 13,
+    title: "13 Climate Action",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 14,
+    title: "14 Life Below Water",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 15,
+    title: "15 Life on Land",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 16,
+    title: "16 Peace, Justice and Strong Institutions",
+    path: "",
+    cName: "submenu-item",
+  },
+  {
+    id: 17,
+    title: "17 Partnerships for the Goals",
+    path: "",
+    cName: "submenu-item",
+  },
+];
 
-]
-
-export default DropdownMenu;
+export default Dropdown;
