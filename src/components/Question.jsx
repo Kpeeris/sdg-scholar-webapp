9d05d7b3bcd39f815a1401768ec07a257cda90e6
dithintheafternoon <sadeng@student.unimelb.edu.au>
2024-09-29 16:11:20 +1000

    create doable, dynamic quizzes wtihout the markng functionality

---
+++ src/components/Question.jsx
@@ -0,0 +1,54 @@
+import { React, useEffect, useState } from 'react'
+import { Checkbox } from "@/components/ui/checkbox"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button, buttonVariants } from "@/components/ui/button"
+
+//please take userNumber out later once we've figured out how to save global variables for user auth
+const Question = ({ q, ans }) => {  {/*, userNumber*/}
+    //ans[q.questionNumber] = {}
+
+    const [answer, setAnswer] = useState("")
+
+    useEffect(() => {
+        saveProgress()
+    }, [answer])
+
+    const saveProgress = () => {
+        ans[q.questionNumber] = {}
+        //ans[q.questionNumber][] = answer
+    }
+    
+
+
+    return (
+        <Card >
+            <CardHeader>
+                <p style={{fontWeight:"bold"}}>Question { q.questionNumber }</p>
+                <p>{ q.questionText }</p>
+            </CardHeader>
+
+            <div style={{paddingLeft:"40px", paddingRight:"40px", paddingBottom:"30px"}}>
+                {Object.values(q.options).map((option, index)=>{
+                    return (
+                        <div key={index} className="flex items-center space-x-2">
+                            <Checkbox id="terms2" />
+                            <label
+                                htmlFor="terms2"
+                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                                {option}
+                            </label>
+                            <br/>
+                        </div>
+                    )
+                    
+                })}
+            </div>
+
+        </Card>
+    )
+
+
+}
+
+export default Question
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,32 +1,110 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import SideMenu from "../components/SideMenu";
 import "../components/SideMenu.css"
 import {useParams} from "react-router-dom"
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import db from '../../firebaseFiles/firebaseConfig.js';
+import Question from "../components/Question.jsx"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 const Quiz = () => {
 
     const { moduleId } = useParams();
-    const moduleTitle = `Module ${moduleId}`;
+    const moduleTitle = `Target 11.${moduleId} Quiz`;
+    //const [nQuestions, setNQuestions] = useState(1)
+    const [docs, setDocs] = useState({})
+    const ans = {}
+
+
+    const getTotalQuestions = async () => {
+        try {
+            let docRef = doc(db, `quizzes/sdg11t${ moduleId }`)
+            let docSnap = await getDoc(docRef)
+
+            if (docSnap.exists()) {
+                
+                console.log((docSnap.data()).totalQuestions)
+                return (docSnap.data()).totalQuestions
+            } else {
+                console.log('nQuestions Document does not exist')
+            }
+
+            
+        } catch (e) {
+            console.error('Error retrieving document: ', e)
+        }
+        return 0
+    }
+
+    const getQuestions = async () => {
+        const totalQuestions = await getTotalQuestions()
+
+        try {
+            let docRef = doc(db, `quizzes/sdg11t${ moduleId }`)
+            let docSnap = await getDoc(docRef)
+
+            if (docSnap.exists()) {
+                const newDocs = {}
+                //console.log((docSnap.data()).totalQuestions)
+                //setNQuestions((docSnap.data()).totalQuestions)
+                for (let i = 1; i<= totalQuestions; i++) {
+                    let docRef = doc(db, `quizzes/sdg11t${ moduleId }/questions/sdg11t${ moduleId }q${i}`)
+                    let docSnap = await getDoc(docRef)
+            
+                    if (docSnap.exists()) {
+                        console.log((docSnap.data()).questionText)
+                        newDocs[`sdg11t${ moduleId }q${i}`] = docSnap.data()
+                        console.log(`document sdg11t${ moduleId }q${i} is now ${ docSnap.data().questionText }`)
+                        console.log("newdocs is: "+ (newDocs[`sdg11t${ moduleId }q${i}`]).questionText)
+                    } else {
+                        console.log(`Document does not exist at sdg11t${ moduleId }q${i}`)
+                    }
+                }
+                setDocs(newDocs)
+            } else {
+                console.log('nQuestions Document does not exist')
+            }
+
+            
+        } catch (e) {
+            console.error('Error retrieving document: ', e)
+        }
+    }
+
+    useEffect(() => {
+        getQuestions()
+    }, [])
+
+    const checkAnswers = (ans) => {
+        
+    }
 
     return <div className="main-content">
-        <h2>Quiz Page</h2>
+        <h2>{ moduleTitle }</h2>
+        <br />
         <SideMenu moduleTitle={`Target 11.${moduleId}`} moduleId={moduleId} />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ab sunt aliquam suscipit dolor inventore laborum doloribus, aliquid consequuntur laudantium nam ipsa voluptates porro ad est incidunt impedit esse corrupti, necessitatibus accusantium. Qui quam sed beatae dignissimos enim officiis nam, voluptates molestias esse cumque magni similique nobis a magnam perferendis perspiciatis? Eligendi corrupti quod laboriosam maxime velit consequatur, veritatis similique voluptates, dolorem alias impedit ducimus commodi error! Voluptatem fuga adipisci at, nostrum quo rem, quibusdam beatae laborum magni, eaque recusandae? Cumque neque ullam hic, vitae iure nulla similique unde eaque cum omnis? Rerum perferendis culpa tempore, saepe officia mollitia sed!</p>
-        <br />
+        <div>
+            {Object.values(docs).map((question) => {
+                console.log("question number is: "+ question.questionNumber)
+                console.log("value at docs is: "+ question.questionText)
+                return (
+                    <div>
+                    <Question key={question.questionNumber} q={ question } ans={ ans }/>
+                    <br />
+                    </div>
+                )
+                
+            }
+                )
+                
+            }
+        </div>
+
+        <div className="flex justify-center">
+            <Button onClick={ () => checkAnswers() } className="w-44">Submit Quiz</Button>
+        </div>
+        
+        
     </div>
 }
 export default Quiz
