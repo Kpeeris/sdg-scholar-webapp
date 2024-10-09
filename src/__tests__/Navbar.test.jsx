6e2e1e7a3d1c2e4cdaf645eb85dc1bf4b86ca730
Kpeeris <kpeeris@student.unimelb.edu.au>
2024-10-09 13:33:25 +1100

    test: test is dropdown works

--- src/__tests__/Navbar.test.jsx
+++ src/__tests__/Navbar.test.jsx
@@ -1,15 +1,16 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 import Navbar from "@/components/Navbar/Navbar";
-import { describe, it, beforeEach } from "vitest";
+import { describe, it, beforeEach, expect } from "vitest";
 
 describe("Navbar Component", () => {
   describe("test rendering", () => {
     beforeEach(() => {
       render(
-        <BrowserRouter>
+        <MemoryRouter>
           <Navbar />
-        </BrowserRouter>
+        </MemoryRouter>
       );
     });
 
@@ -17,31 +18,5 @@ describe("Navbar Component", () => {
       const logo = screen.getByAltText(/sdg logo/i);
       expect(logo).toBeInTheDocument();
     });
-  });
-
-  describe("test routing in navbar", () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter initialEntries={["/"]}>
-          <Navbar />
-        </MemoryRouter>
-      );
-    });
-    it("navigates to Home page when clicking the SDG logo", () => {
-      const logo = screen.getByAltText(/sdg logo/i);
-      fireEvent.click(logo);
-      expect(window.location.pathname).toBe("/");
-    });
-    it("navigates to Home page when clicking the Home link", () => {
-      const home = screen.getByTestId("home-button");
-      fireEvent.click(home);
-      expect(window.location.pathname).toBe("/");
-    });
-
-    // it("navigates to About page when clicking the About link", () => {
-    //   const about = screen.getByTestId("about-button");
-    //   fireEvent.click(about);
-    //   expect(window.location.pathname).toBe("/about");
-    // });
   });
 });
