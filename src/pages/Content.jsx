89b53c5ea3622d66da6c7e88b3628fe3a22c3800
dithintheafternoon <sadeng@student.unimelb.edu.au>
2024-10-16 02:32:25 +1100

    feat: connected to firestorage. pulls images from fire storage to content page. content editor box now saves all formatting avialble in tool bar. not sure why the links arent blue

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react"
 import SideMenu from "../components/SideMenu";
-//import "../components/SideMenu.css";
 import {useParams} from "react-router-dom";
 import EditableBlock from '../components/EditableBlock';
 
+//import storage from '../../firebaseFiles/firebaseConfig.js';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 const Content = () => {
     const { moduleId } = useParams(); // Capture the module ID from the URL
+    const [image1Url, setImage1Url] = useState("")
+    const [image2Url, setImage2Url] = useState("")
+
+    const storage = getStorage()
     
     let dbModuleId = moduleId
     if(moduleId === 'a'){
@@ -15,6 +22,45 @@ const Content = () => {
         dbModuleId = '10'
     }
 
+    const retrieveImages = async () => {
+        //const dbString = `/sdg11/target${dbModuleId}/GOAL_11_TARGET_11.1.png`
+        const testDbString= "New Ardoch Logo (1) 1.png"
+        //console.log(`trying to hit: ${dbString}`)
+        console.log(`${isNaN(Number(dbModuleId))}`)
+        try{
+            getDownloadURL(ref(storage, `/sdg11/target${dbModuleId}/GOAL_11_TARGET_11.${isNaN(Number(moduleId)) ? moduleId.toUpperCase() : dbModuleId}.png`)).then((url) => {
+                
+                setImage1Url(url)
+            })
+            getDownloadURL(ref(storage, `sdg11/target${dbModuleId}/MC_Target_11.${isNaN(Number(moduleId)) ? moduleId.toUpperCase() : dbModuleId}.png`)).then((url) => {
+                console.log(url)
+                setImage2Url(url)
+            })
+
+        } catch (error) {
+            console.error("Error retrieving images:", error);
+        
+            // Handling different error types
+            if (error.code === 'storage/object-not-found') {
+                console.error('The file does not exist at the specified path.');
+            } else if (error.code === 'storage/unauthorized') {
+                console.error('User does not have permission to access the file.');
+            } else {
+                console.error('Unknown error occurred:', error.message);
+            }
+        }
+        //const storageRef1 = ref(storage, `/sdg11/target${dbModuleId}/GOAL_11_TARGET_11.1.png`)
+        //const storageRef2 = ref(storage, `/sdg11/target${dbModuleId}/MC_Target_11.1.png`)
+
+        
+    }
+
+    useEffect(() => {
+        retrieveImages()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        console.log(`url of image 1 is ${image1Url}. url of mage 2 is ${image2Url}`)
+    }, [])
+
     // You can replace this with logic to dynamically retrieve module info
     const moduleTitle = `Target 11.${moduleId}`; 
 
@@ -23,8 +69,11 @@ const Content = () => {
         <SideMenu moduleTitle={moduleTitle} moduleId={moduleId}/>
             <div className="ml-[250px] flex-1">
             <h2 style={{ fontWeight: "bold" }}>Content Page</h2>
-            
-
+            <div className="relative h-96 flex flex-cols">
+                <img src={ image1Url } alt="Image 1" />
+                <img src={ image2Url } alt="Image 2" />
+            </div>
+            <br/>
             <div>
                 <EditableBlock moduleId={ dbModuleId } />
             </div>
