e6d5725c2c46c4985d3f017bbd1a1fc4a07acfff
Kpeeris <kpeeris@student.unimelb.edu.au>
2024-10-16 01:12:59 +1100

    fix: title font in content page

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,37 +1,37 @@
 import SideMenu from "../components/SideMenu";
 //import "../components/SideMenu.css";
-import {useParams} from "react-router-dom";
-import EditableBlock from '../components/EditableBlock';
+import { useParams } from "react-router-dom";
+import EditableBlock from "../components/EditableBlock";
 
 const Content = () => {
-    const { moduleId } = useParams(); // Capture the module ID from the URL
-    
-    let dbModuleId = moduleId
-    if(moduleId === 'a'){
-        dbModuleId = '8'
-    } else if(moduleId === 'b'){
-        dbModuleId = '9'
-    } else if(moduleId === 'c'){
-        dbModuleId = '10'
-    }
+  const { moduleId } = useParams(); // Capture the module ID from the URL
 
-    // You can replace this with logic to dynamically retrieve module info
-    const moduleTitle = `Target 11.${moduleId}`; 
+  let dbModuleId = moduleId;
+  if (moduleId === "a") {
+    dbModuleId = "8";
+  } else if (moduleId === "b") {
+    dbModuleId = "9";
+  } else if (moduleId === "c") {
+    dbModuleId = "10";
+  }
 
-    return (
-        <div className="flex">
-        <SideMenu moduleTitle={moduleTitle} moduleId={moduleId}/>
-            <div className="ml-[250px] flex-1">
-            <h2 style={{ fontWeight: "bold" }}>Content Page</h2>
-            
+  // You can replace this with logic to dynamically retrieve module info
+  const moduleTitle = `Target 11.${moduleId}`;
 
-            <div>
-                <EditableBlock moduleId={ dbModuleId } />
-            </div>
-            </div>
-        
+  return (
+    <div className="flex">
+      <SideMenu moduleTitle={moduleTitle} moduleId={moduleId} />
+      <div className="ml-[250px] flex-1">
+        <h2 style={{ fontSize: "3rem", lineHeight: "1rem" }}>
+          {moduleTitle} Content
+        </h2>
+
+        <div className="mt-12">
+          <EditableBlock moduleId={dbModuleId} />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Content
+export default Content;
