49bf198998f98bb44422ff5abc96b51bf59d66a5
kaylynthomso <kaylynthomson.phan@student.unimelb.edu.au>
2024-10-23 01:44:27 +1100

    feat: confirm password for sign up flow

--- src/pages/signup/components/SignUpForm.jsx
+++ src/pages/signup/components/SignUpForm.jsx
@@ -3,15 +3,22 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline"; 
 
-export const SignUpForm = ({ firstNameRef, lastNameRef, emailRef, passwordRef }) => {
-  const [passwordVisible, setPasswordVisible] = useState(false); // Track password visibility
+export const SignUpForm = ({ firstNameRef, lastNameRef, emailRef, passwordRef, confirmPasswordRef }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false); 
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible); // Toggle visibility state
+    setPasswordVisible(!passwordVisible); 
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible); 
   };
 
   return (
     <div className="space-y-6">
+      
+      {/* First name and last name div */}
       <div className="flex justify-center items-center space-x-5">
         <div className="w-full flex flex-col items-left gap-2">
           <Label htmlFor="first-name">First Name</Label>
@@ -24,11 +31,13 @@ export const SignUpForm = ({ firstNameRef, lastNameRef, emailRef, passwordRef })
         </div>
       </div>
 
+      {/* Email div */}
       <div className="w-full flex flex-col items-left gap-2">
         <Label htmlFor="email">Email</Label>
         <Input type="email" id="email" placeholder="Email" ref={emailRef} />
       </div>
 
+      {/* Password and password rules div */}
       <div className="space-y-3">
         <div className="w-full flex flex-col items-left gap-2">
           <Label htmlFor="password">Password</Label>
@@ -61,6 +70,33 @@ export const SignUpForm = ({ firstNameRef, lastNameRef, emailRef, passwordRef })
           <li>At least 1 number and special character</li>
         </ul>
       </div>
+
+      {/* Confirm password div */}
+      <div className="w-full flex flex-col items-left gap-2 pb-4">
+          <Label htmlFor="password">Confirm Password</Label>
+          <div className="relative">
+            <Input
+              type={confirmPasswordVisible ? "text" : "password"} // Toggle between text and password
+              id="confirm-password"
+              placeholder="Confirm Password"
+              ref={confirmPasswordRef}
+            />
+
+            {/* Eye Icon Button to toggle visibility */}
+            <button
+              type="button"
+              className="absolute right-0 top-0 mt-3 mr-3"
+              onClick={toggleConfirmPasswordVisibility}
+            >
+              {confirmPasswordVisible ? (
+                <EyeSlashIcon className="h-5 w-5 text-gray-400" />
+              ) : (
+                <EyeIcon className="h-5 w-5 text-gray-400" />
+              )}
+            </button>
+          </div>
+        </div>
+
     </div>
   );
 };
