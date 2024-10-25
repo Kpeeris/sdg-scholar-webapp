21e04184fbad0c1639d0c31f42574c3fe98e0d5e
Kpeeris <kpeeris@student.unimelb.edu.au>
2024-10-25 20:23:11 +1100

    test: signup document write for learner and admin

--- src/__tests__/Signup.test.jsx
+++ src/__tests__/Signup.test.jsx
@@ -1,7 +1,8 @@
 import SignUp from "@/pages/signup/SignUp";
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import { signup } from "../../firebaseFiles/firebaseAuth";
-import { MemoryRouter } from "react-router-dom";
+import { doc, setDoc, serverTimestamp, getFirestore } from "firebase/firestore";
+import { MemoryRouter, useLocation } from "react-router-dom";
 import { afterEach } from "vitest";
 import { it, expect, describe, vi } from "vitest";
 
@@ -9,13 +10,20 @@ vi.mock("../../firebaseFiles/firebaseAuth", () => ({
   signup: vi.fn(),
 }));
 
+vi.mock("firebase/firestore", () => {
+  return {
+    getFirestore: vi.fn(),
+    doc: vi.fn(),
+    setDoc: vi.fn(),
+    serverTimestamp: vi.fn(),
+  };
+});
+
 vi.mock("react-router-dom", async () => {
   const actual = await vi.importActual("react-router-dom");
   return {
     ...actual,
-    useLocation: () => ({
-      state: { userType: "learner" }, // Mock userType as learner
-    }),
+    useLocation: vi.fn(),
   };
 });
 
@@ -25,6 +33,9 @@ describe("SignUp Page", () => {
   });
 
   it("should render the signup page", async () => {
+    useLocation.mockReturnValue({
+      state: { userType: "learner" },
+    });
     render(
       <MemoryRouter>
         <SignUp />
@@ -45,10 +56,13 @@ describe("SignUp Page", () => {
     expect(signupButton).toBeInTheDocument();
   });
 
-  it("should signup Learner successfully", async () => {
+  it("should successfully call login to signup learner using firebase authentication", async () => {
     //mock successful signup
     signup.mockResolvedValueOnce({ success: true });
 
+    useLocation.mockReturnValue({
+      state: { userType: "learner" },
+    });
     render(
       <MemoryRouter>
         <SignUp />
@@ -78,23 +92,22 @@ describe("SignUp Page", () => {
     expect(emailInput.value).toBe("test@test.com");
     expect(passwordInput.value).toBe("Abcd1234");
 
-    // console.log("before click");
-    // screen.debug();
     //click signup button
     fireEvent.click(signupButton);
-    // console.log("after click");
-    // screen.debug();
 
     await waitFor(() => {
+      expect(signup).toHaveBeenCalledWith("test@test.com", "Abcd1234");
       expect(signup).toHaveBeenCalledTimes(1);
     });
-
-    expect(signup).toHaveBeenCalledWith("test@test.com", "Abcd1234");
   });
 
   it("should show error message on signup failure", async () => {
     signup.mockResolvedValueOnce({
       code: "auth/password-does-not-meet-requirements",
+    });
+
+    useLocation.mockReturnValue({
+      state: { userType: "learner" },
     });
     render(
       <MemoryRouter>
@@ -118,6 +131,111 @@ describe("SignUp Page", () => {
 
     await waitFor(() => {
       expect(screen.getByTestId("signupErrorMessage")).toBeInTheDocument();
+    });
+  });
+
+  //todo test that we are saving the correct user information to the database
+  it("should create a document in the database that corresponds to the new learner", async () => {
+    signup.mockResolvedValueOnce({
+      user: { uid: "12345", email: "learner@gmail.com" },
+    });
+
+    useLocation.mockReturnValue({
+      state: { userType: "learner" },
+    });
+    //moch firestore document write
+    setDoc.mockResolvedValueOnce();
+
+    render(
+      <MemoryRouter>
+        <SignUp />
+      </MemoryRouter>
+    );
+
+    const firstNameInput = screen.getByTestId("first-name");
+    const lastNameInput = screen.getByTestId("last-name");
+    const emailInput = screen.getByTestId("signup-email");
+    const passwordInput = screen.getByTestId("signup-password");
+    const signupButton = screen.getByTestId("signup-button");
+
+    fireEvent.change(firstNameInput, { target: { value: "Test" } });
+    fireEvent.change(lastNameInput, { target: { value: "learner" } });
+    fireEvent.change(emailInput, { target: { value: "learner@gmail.com" } });
+    fireEvent.change(passwordInput, { target: { value: "Pass_word123" } });
+
+    fireEvent.click(signupButton);
+    //screen.debug();
+
+    await waitFor(() => {
+      expect(signup).toHaveBeenCalledWith("learner@gmail.com", "Pass_word123");
+      expect(setDoc).toHaveBeenCalledTimes(1);
+      expect(setDoc).toHaveBeenCalledWith(
+        doc(expect.anything(), "learners", "12345"),
+        {
+          email: "learner@gmail.com",
+          firstName: "Test",
+          lastName: "learner",
+          createdAt: serverTimestamp(),
+          scores: {
+            sdg11t1: 0,
+            sdg11t2: 0,
+            sdg11t3: 0,
+            sdg11t4: 0,
+            sdg11t5: 0,
+            sdg11t6: 0,
+            sdg11t7: 0,
+            sdg11t8: 0,
+            sdg11t9: 0,
+            sdg11t10: 0,
+          },
+        }
+      );
+    });
+  });
+
+  it("should create a document in the database that corresponds to the new admin", async () => {
+    signup.mockResolvedValueOnce({
+      user: { uid: "123456", email: "admin@gmail.com" },
+    });
+
+    useLocation.mockReturnValue({
+      state: { userType: "admin" },
+    });
+    //moch firestore document write
+    setDoc.mockResolvedValueOnce();
+
+    render(
+      <MemoryRouter>
+        <SignUp />
+      </MemoryRouter>
+    );
+
+    const firstNameInput = screen.getByTestId("first-name");
+    const lastNameInput = screen.getByTestId("last-name");
+    const emailInput = screen.getByTestId("signup-email");
+    const passwordInput = screen.getByTestId("signup-password");
+    const signupButton = screen.getByTestId("signup-button");
+
+    fireEvent.change(firstNameInput, { target: { value: "Test" } });
+    fireEvent.change(lastNameInput, { target: { value: "admin" } });
+    fireEvent.change(emailInput, { target: { value: "admin@gmail.com" } });
+    fireEvent.change(passwordInput, { target: { value: "Pass_word123" } });
+
+    fireEvent.click(signupButton);
+    //screen.debug();
+
+    await waitFor(() => {
+      expect(signup).toHaveBeenCalledWith("admin@gmail.com", "Pass_word123");
+      expect(setDoc).toHaveBeenCalledTimes(1);
+      expect(setDoc).toHaveBeenCalledWith(
+        doc(expect.anything(), "admins", "123456"),
+        {
+          email: "admin@gmail.com",
+          firstName: "Test",
+          lastName: "admin",
+          createdAt: serverTimestamp(),
+        }
+      );
     });
   });
 });
