fd192306ce4e602330e47ae7c4f5b75c7dd99193
Kpeeris <kpeeris@student.unimelb.edu.au>
2024-11-01 13:51:02 +1100

    test: incrased timeout on waitFor function

--- src/__tests__/Quiz.test.jsx
+++ src/__tests__/Quiz.test.jsx
@@ -17,7 +17,7 @@ import {
   collection,
   getDocs,
 } from "firebase/firestore";
-//import { setUpData, cleanUpData } from "./dbUtils";
+import { setUpData, cleanUpData } from "./dbUtils";
 
 // Mock the useAuthContext hook
 vi.mock("@/AuthProvider", () => {
@@ -35,158 +35,6 @@ vi.mock("react-router-dom", async () => {
     useLocation: vi.fn(),
   };
 });
-
-const setUpData = async () => {
-  await setDoc(doc(db, "learners", "learner1"), {
-    email: "test_learner@example.com",
-    firstName: "Test",
-    lastname: "Learner",
-    scores: { sdg11t1: 80, sdg11t2: 100, sdg11t3: 0 },
-  });
-  await setDoc(doc(db, "learners", "learner2"), {
-    email: "learner_test@example.com",
-    firstName: "learner_two",
-    lastname: "New",
-    scores: { sdg11t1: 0, sdg11t2: 0, sdg11t3: 0 },
-  });
-  await setDoc(doc(db, "admins", "admin1"), {
-    email: "test_admin@example.com",
-    firstName: "Test",
-    lastname: "Admin",
-  });
-  const testQuizdata = [
-    {
-      quizId: "sdg11t1",
-      questions: [
-        {
-          id: "Q1",
-          questionText: "What is the capital of France?",
-          options: ["London", "Kenya", "Paris", "Dublin"],
-          correctAnswers: ["Paris"],
-          type: "mcq",
-        },
-        {
-          id: "Q2",
-          questionText: "What is the capital of Germany?",
-          options: ["Sydney", "Berlin", "Jakarta", "Chennai"],
-          correctAnswers: ["Berlin"],
-          type: "mcq",
-        },
-        {
-          id: "Q3",
-          questionText: "Why was 6 scared of 7?",
-          options: ["because", "7", "8", "9"],
-          correctAnswers: ["7", "8", "9"],
-          type: "ms",
-        },
-      ],
-    },
-    {
-      quizId: "sdg11t2",
-      questions: [
-        {
-          id: "Q1",
-          questionText: "What does the fox say?",
-          options: [
-            "Ring-ding-ding-ding-dingeringeding!",
-            "Wa-pa-pa-pa-pa-pa-pow!",
-            "Joff-tchoff-tchoffo-tchoffo-tchoff!",
-            "None of the above",
-          ],
-          correctAnswers: ["None of the above"],
-          type: "mcq",
-        },
-        {
-          id: "Q2",
-          questionText: "Who let the dogs out?",
-          options: [
-            "Baha Men",
-            "Snoop Dog",
-            "ur mum",
-            "the dog let itself out",
-          ],
-          correctAnswers: ["Baha Men"],
-          type: "mcq",
-        },
-        {
-          id: "Q3",
-          questionText: "This is a multiple select question",
-          options: ["pick", "multiple", "answers", "another answer"],
-          correctAnswers: ["pick", "multiple", "answers"],
-          type: "ms",
-        },
-      ],
-    },
-    {
-      quizId: "sdg11t3",
-      questions: [
-        {
-          id: "Q1",
-          questionText: "Hotel?",
-          options: ["Trivago", "Tripadvisor", "airbnb", "booking.com"],
-          correctAnswers: ["Trivago"],
-          type: "mcq",
-        },
-        {
-          id: "Q2",
-          questionText: "What is the capital of Germany?",
-          options: ["London", "Berlin", "Paris", "Dublin"],
-          correctAnswers: ["Berlin"],
-          type: "mcq",
-        },
-        {
-          id: "Q3",
-          questionText: "Can I be bothered thinking of another question?",
-          options: [
-            "clearly not",
-            "not correct",
-            "also not correct",
-            "I should have used the Qs we alaredy had",
-          ],
-          correctAnswers: [
-            "I should have used the Qs we alaredy had",
-            "clearly not",
-          ],
-          type: "ms",
-        },
-      ],
-    },
-  ];
-
-  for (const quiz of testQuizdata) {
-    const quizRef = doc(db, "quizzes", quiz.quizId);
-    for (const question of quiz.questions) {
-      await setDoc(doc(quizRef, "questions", question.id), {
-        questionText: question.questionText,
-        options: question.options,
-        correctAnswers: question.correctAnswers,
-        type: question.type,
-      });
-    }
-  }
-  console.log("data set up");
-};
-
-const cleanUpData = async () => {
-  await deleteDoc(doc(db, "learners", "learner1"));
-  await deleteDoc(doc(db, "learners", "learner2"));
-
-  await deleteDoc(doc(db, "admins", "admin1"));
-
-  const quixIds = ["sdg11t1", "sdg11t2", "sdg11t3"];
-  for (const quizId of quixIds) {
-    const quizRef = doc(db, "quizzes", quizId);
-
-    const questions = await getDocs(collection(quizRef, "questions"));
-    for (const question of questions.docs) {
-      await deleteDoc(question.ref);
-    }
-    //const deletePromises = questions.docs.map((doc) => deleteDoc(doc.ref));
-    //await Promise.all(deletePromises);
-    await deleteDoc(quizRef);
-  }
-  console.log("data cleaned up");
-};
 
 // Render the Quiz component with the given user role, email and test number
 const renderComponent = (userRole, userEmail, TargetNum) => {
@@ -218,9 +66,9 @@ describe("Quiz Component", () => {
     vi.clearAllMocks();
   });
 
-  // afterAll(async () => {
-  //   await cleanUpData();
-  // });
+  afterAll(async () => {
+    await cleanUpData();
+  });
 
   it("should render the score page if the learner has already has a score,", async () => {
     renderComponent("learner", "test_learner@example.com", "1");
@@ -234,18 +82,24 @@ describe("Quiz Component", () => {
   it("should render the pre-quiz page if the learner has 0 score,", async () => {
     renderComponent("learner", "learner_test@example.com", "1");
 
-    await waitFor(() => {
-      expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
-      expect(screen.getByText("Ready for the Quiz?")).toBeInTheDocument();
-    });
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
+        expect(screen.getByText("Ready for the Quiz?")).toBeInTheDocument();
+      },
+      { timeout: 5000 }
+    );
   });
 
   it("should render questions when learner clicks start quiz from pre-quiz page", async () => {
     renderComponent("learner", "learner_test@example.com", "1");
 
-    await waitFor(() => {
-      expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
-    });
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
+      },
+      { timeout: 5000 }
+    );
 
     const startQuizButton = screen.getByTestId("startQuizButton");
 
@@ -275,9 +129,12 @@ describe("Quiz Component", () => {
   it("should NOT render edit quiz button for learner", async () => {
     renderComponent("learner", "learner_test@example.com", "1");
 
-    await waitFor(() => {
-      expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
-    });
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
+      },
+      { timeout: 5000 }
+    );
 
     //click start quiz button
     const startQuizButton = screen.getByTestId("startQuizButton");
@@ -303,9 +160,12 @@ describe("Quiz Component", () => {
   it("should render confirmation dialog if learner clicks submit quiz button ", async () => {
     renderComponent("learner", "learner_test@example.com", "1");
 
-    await waitFor(() => {
-      expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
-    });
+    await waitFor(
+      () => {
+        expect(screen.getByTestId("preQuizPage")).toBeInTheDocument();
+      },
+      { timeout: 5000 }
+    );
 
     // Navigate to questions page
     const startQuizButton = screen.getByTestId("startQuizButton");
